a71f20c27a2ccaf28f94445b5b1c0e4594b00194
ShannaJSmith <shanna_smith987@hotmail.com>
2021-09-22 17:29:46 +0000

    refactored play file to export connect function using E6S shorthand syntax

---
+++ client.js
@@ -0,0 +1,24 @@
+const net = require("net");
+const connect = function () {
+  const conn = net.createConnection({
+    host: '135.23.223.133',
+    port: '50542'
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on("connect", () => {
+    console.log('Connection Established!');
+  })
+  conn.on('data', (data) => {
+    console.log(data.toString());
+    conn.end();
+  })
+  conn.on('end', () => {
+    console.log('You disconnected!');
+  })
+
+  return conn;
+};
+
+module.exports = {connect}
--- play.js
+++ play.js
@@ -1,27 +1,4 @@
-const net = require("net");
+const {connect} = require('./client');
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '135.23.223.133',
-    port: '50542'
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on("connect", () => {
-    console.log('Connection Established!');
-  })
-  conn.on('data', (data) => {
-    console.log(data.toString());
-    conn.end();
-  })
-  conn.on('end', () => {
-    console.log('You disconnected!');
-  })
-
-  return conn;
-};
-
-console.log("Connecting ...");
+console.log("Conecting ...");
 connect();
